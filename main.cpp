c150124b9fd4e82e30f22d34fd24fc1454c05d25
fttsebb <124256325+fttsebb@users.noreply.github.com>
2023-08-07 17:22:07 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+
+
+
 void uzupelnij_tablice(int *tab, int ile);
 void wypisz_tablice(int *tab, int ile);
 int licz_sume_tablicy(int *tab, int ile);
@@ -6,13 +10,25 @@ int znajdz_najwieksza_wartosc(int *tab, int ile);
 int* stworz_tablice(int ile);
 int* odwroc_tablice(int *tab, int ile);
 bool palindrome(int *tab, int ile);
-
+int NWD(int liczba1, int liczba2);
+std::vector<int> zamiana_binarna(int liczba);
 
 using namespace std;
 
+vector<int> zamiana_binarna(int liczba){
+    vector<int> liczba_binarna;
+    while (liczba > 0){
+        liczba_binarna.push_back(liczba % 2);
+        liczba /= 2;
+    }
+    return liczba_binarna;
+}
+
+
+
 int main()
 {
-    cout << "ile chcesz stworzyc elementow w tablicy?";
+    /*cout << "ile chcesz stworzyc elementow w tablicy?";
     int ile;
     cin >> ile;
     int * tablica = stworz_tablice(ile);
@@ -21,9 +37,19 @@ int main()
     int* tablica2 = odwroc_tablice(tablica, ile);
     //wypisz_tablice(tablica2, ile);
     cout << palindrome(tablica, ile);
-
     delete[] tablica2;
     delete[] tablica;
+
+*/
+    //binarna liczba
+    int liczba = 10;
+    vector<int> binarna = zamiana_binarna(liczba);
+    for (int i = 0 ; i < binarna.size(); i++){
+        cout << binarna[i];
+    }
+
+    //NWD
+    NWD(282, 78);
     return 0;
 }
 
@@ -88,3 +114,66 @@ bool palindrome(int *tab, int ile){
     }
 
 }
+
+long int potega(int p, int w) {
+    if (w == 0) return 1;
+    else return p * potega(p, w - 1);
+}
+
+long int fib(int n) {
+    if (n == 1 || n == 2) return 1;
+    else return fib(n - 1) + fib(n - 2);
+}
+
+long int silnia(int n) {
+    if (n==0) return 1;
+    else return n*silnia(n-1);
+
+}
+
+int NWD(int liczba1, int liczba2){
+    //algorytm euklidesa
+    int pom1;
+    int temp = 0;
+    if (liczba1 % liczba2 == 0){
+        cout << "NWD TO: " << liczba2 << endl;
+        return 1;
+    }
+    else{
+        do {
+            temp = liczba1 % liczba2;
+            if(temp != 0) {
+                pom1 = liczba2;
+                liczba1 = pom1;
+                liczba2 = temp;
+                cout << temp << endl;
+            }
+            if(temp == 0){
+                cout << "NWD = " << liczba2;
+            }
+        }
+        while (temp != 0);
+    }
+}
+
+
+int liczby_pierwsze(int ilosc_liczb){
+    //funkcja, która znajdzie wszystkie liczby pierwsze w zakresie od 2 do podanej przez uzytkownika
+    // czyli liczba ktora jest > 1 i ma tylko dwa dzielniki: 1 i samą siebie
+    int* tablica = new int[ilosc_liczb];
+    for(int i = 0; i < ilosc_liczb; i++)
+    {
+        tablica[i] = i;
+    }
+    for(int k = 0; k < ilosc_liczb; k++){
+        if(tablica[k] % 3 )
+    }
+    delete[] tablica;
+
+
+
+
+}
+
+
+
